3a3d84ea01bf6194bb1024a077e3c1b4a8482603
Timothy <enguji123@gmail.com>
2024-07-29 00:51:35 -0700

    Created Ingredient, Notes, Recipe, and UnitOfMeasure. Added JPA relationships and functionality.

---
+++ src/main/java/domain/UnitOfMeasure.java
@@ -0,0 +1,32 @@
+package domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String uom;
+
+    public String getUom() {
+        return uom;
+    }
+
+    public void setUom(String uom) {
+        this.uom = uom;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
